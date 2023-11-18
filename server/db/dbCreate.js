195da59f56ba981b41b90b274d7cd095b7384f2a
Kameron Keller <mrkameronkeller@gmail.com>
2023-11-18 14:26:30 -0800

    Adds function for scanning the table

--- server/db/dbCreate.js
+++ server/db/dbCreate.js
@@ -1,4 +1,4 @@
-import { DynamoDB } from "@aws-sdk/client-dynamodb";
+import { DynamoDB, ScanCommand } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient, PutCommand } from "@aws-sdk/lib-dynamodb";
 
 var dynamodb = new DynamoDB({ region: 'local', endpoint: "http://localhost:8000"});
@@ -32,4 +32,38 @@ export function createTable() {
 
 export function dbPut(params) {
     docClient.send(new PutCommand(params));
+}
+
+// export function dbScan(params) {
+//     dynamodb.scan(params, function (err, data) {
+//         if (err) {
+//           console.log("Error", err);
+//         } else {
+//           console.log("Success", data);
+//           const jsonString = JSON.stringify(data);
+//           console.log(jsonString);
+//         //   data.Items.forEach(function (element, index, array) {
+//         //     console.log(
+//         //         "printing",
+//         //         element.sk.S
+//         //     );
+//         //   });
+//         }
+//       });
+// }
+
+export async function dbScan(params) {
+    const data = await docClient.send(new ScanCommand(params));
+    try {
+        return data.Items.map(item => ({
+            name: item.truckName.S,
+            type: item.type.S,
+            hours: item.hours.S,
+            lot: item.sk.S,
+            address: item.address.S
+        }));
+        } catch (error) {
+        console.error('Error retrieving items:', error);
+        throw error;
+        }
 }
